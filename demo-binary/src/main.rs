a1b674bf64e172d1a41d863c4fa72f206ae059a4
Luca Barbato <lu_zero@gentoo.org>
2023-03-03 09:12:55 +0100

    Update the demo code

--- demo-binary/src/main.rs
+++ demo-binary/src/main.rs
@@ -1,15 +1,19 @@
-use sifis_api::{Context, Light, Percentage, Rgb};
+use sifis_api::Sifis;
 
-pub fn turn_light_on() -> i32 {
-    0
-}
+#[tokio::main]
+async fn main() -> Result<(), sifis_api::Error> {
+    let sifis = Sifis::new().await?;
+    let lamps = sifis.lamps().await?;
 
-fn main() {
-    let context = Context::default();
-    let mut devices = context.find_all::<Light>().unwrap();
-    for device in &mut devices {
-        let brightness = Percentage::new(30);
-        let color = Rgb::new(255, 0, 0);
-        device.turn_light_on(brightness, color).unwrap();
+    for lamp in lamps {
+        let on_off: &str = if lamp.get_on_off().await? {
+            "On"
+        } else {
+            "Off"
+        };
+        let brightness: u8 = lamp.get_brightness().await?;
+        println!("{:<15} {:<7} {:<5} ", lamp.id, on_off, brightness);
     }
+
+    Ok(())
 }
